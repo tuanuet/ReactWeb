edcff5df5739e8562704cadc43da479fe95b0410
tuanuet <tuan.uet@gmail.com>
2017-06-30 13:31:15 +0700

    fix nav

--- app/app.js
+++ app/app.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import ListLine from './component/ListLine'
-import Homepage from './component/homepage'
 const { Provider } = require('react-redux')
 import store from './store'
 import Login from './component/login'
@@ -15,21 +14,6 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import '../public/css/font-awesome.css'
 import '../public/css/app.css'
 
-<<<<<<< HEAD
-=======
-class App extends React.Component {
-
-  render(){
-    return(
-      <div className="container darktheme">
-        <Homepage />
-      </div>
-    )
-  }
-}
-
-
->>>>>>> 4a2cf8242307e3701f1a0ddbfc84c6fde259e6b1
 ReactDOM.render(
   <Provider store={store}>
     <Router>
--- app/component/layout.js
+++ app/component/layout.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import NavBar from './navbar'
+import NavBar from './header'
 import Footer from './footer'
 class Layout extends React.Component {
   render(){
